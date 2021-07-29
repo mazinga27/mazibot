69aca144faacd8f24306f56553b1bdf2642d2c31
mazinga27 <88095998+mazinga27@users.noreply.github.com>
2021-07-29 22:35:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import functools
 import itertools
 import math
+import datetime
+import discord-components
 import random
 import discord
 import language as language
@@ -557,7 +559,85 @@ class Music(commands.Cog):
         vc = voice
       vc.play(discord.FFmpegPCMAudio("audio.mp3"), after=lambda e: print("Ho Eseguito Il Comando"))
       
-
+client = commands.Bot(command_prefix = '-')
+ 
+@client.event
+async def on_ready():
+	#turns on discord components lib
+    DiscordComponents(client)
+ 
+#buttons array
+buttons = [
+    [
+        Button(style=ButtonStyle.grey, label='1'),
+        Button(style=ButtonStyle.grey, label='2'),
+        Button(style=ButtonStyle.grey, label='3'),
+        Button(style=ButtonStyle.blue, label='×'),
+        Button(style=ButtonStyle.red, label='Exit')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='4'),
+        Button(style=ButtonStyle.grey, label='5'),
+        Button(style=ButtonStyle.grey, label='6'),
+        Button(style=ButtonStyle.blue, label='÷'),
+        Button(style=ButtonStyle.red, label='←')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='7'),
+        Button(style=ButtonStyle.grey, label='8'),
+        Button(style=ButtonStyle.grey, label='9'),
+        Button(style=ButtonStyle.blue, label='+'),
+        Button(style=ButtonStyle.red, label='Clear')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='00'),
+        Button(style=ButtonStyle.grey, label='0'),
+        Button(style=ButtonStyle.grey, label='.'),
+        Button(style=ButtonStyle.blue, label='-'),
+        Button(style=ButtonStyle.green, label='=')
+    ],
+]
+ 
+#calculates answer
+def calculate(exp):
+    o = exp.replace('×', '*')
+    o = o.replace('÷', '/')
+    result = ''
+    try:
+        result = str(eval(o))
+    except:
+        result = 'An error occurred.'
+    return result
+ 
+@client.command()
+async def calc(ctx):
+    m = await ctx.send(content='Loading Calculators...')
+    expression = 'None'
+    delta = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+    e = discord.Embed(title=f'{ctx.author.name}\'s calculator | {ctx.author.id}', description=expression,
+                        timestamp=delta)
+    await m.edit(components=buttons, embed=e)
+    while m.created_at < delta:
+        res = await client.wait_for('button_click')
+        if res.author.id == int(res.message.embeds[0].title.split('|')[1]) and res.message.embeds[
+            0].timestamp < delta:
+            expression = res.message.embeds[0].description
+            if expression == 'None' or expression == 'An error occurred.':
+                expression = ''
+            if res.component.label == 'Exit':
+                await res.respond(content='Calculator Closed', type=7)
+                break
+            elif res.component.label == '←':
+                expression = expression[:-1]
+            elif res.component.label == 'Clear':
+                expression = 'None'
+            elif res.component.label == '=':
+                expression = calculate(expression)
+            else:
+                expression += res.component.label
+            f = discord.Embed(title=f'{res.author.name}\'s calculator|{res.author.id}', description=expression,
+                                timestamp=delta)
+            await res.respond(content='', embed=f, components=buttons, type=7)
 
 bot = commands.Bot('-', description='MaziBot.')
 bot.add_cog(Music(bot))
